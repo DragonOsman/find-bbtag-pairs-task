260cc7f10ca301298173f7022f1c3ebaf28fcd71
Osman Zakir <osmanzakir90@hotmail.com>
2020-02-01 21:06:12 +0500

    added HTML and JS files with markup and code

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const str1 = "..[url]http://google.com[/url]..";
+const str2 = "..[url][b]http://google.com[/b][/url].";
+const regex = /\[((url)|(b))\](.+)(\[\\(\1))\]/gsi;
+alert(str1.match(regex));
+alert(str2.match(regex));
