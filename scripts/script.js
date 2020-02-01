a9a4e20e184a77286c94d4c84c52a982e7a1df08
Osman Zakir <osmanzakir90@hotmail.com>
2020-02-01 21:07:18 +0500

    Forgot to save JS file with code earlier, so now saved and added it again

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,6 @@
 
 const str1 = "..[url]http://google.com[/url]..";
 const str2 = "..[url][b]http://google.com[/b][/url].";
-const regex = /\[((url)|(b))\](.+)(\[\\(\1))\]/gsi;
+const regex = /\[(b|url|quote)\].*?\[\/\1\]/gsi;
 alert(str1.match(regex));
 alert(str2.match(regex));
